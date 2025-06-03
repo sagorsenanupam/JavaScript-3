58e353c604cce18404723d50342fa2ff75264d97
sagorsenanupam <senanupam10@gmail.com>
2025-06-04 00:54:14 +0600

    edited

---
+++ object.js
@@ -0,0 +1,25 @@
+const age = 21;
+const school = "rifle square";
+const isPassed = true;
+// let isDeveloper;
+
+// We define object using {}
+// Non primitive --> basic ar choto kora jabe na
+
+const bottle = {
+  color: "blue",
+  brand: "apple",
+  price: 45,
+  isClean: false,
+};
+
+const subject = {
+  name: "Biology",
+  teacher: "Rashed Alam",
+  examDate: "30 Feb",
+  chapters: ["First", "Second", "Third"],
+  exams: {
+    name: "Final Exam",
+    Marks: 100,
+  },
+};
